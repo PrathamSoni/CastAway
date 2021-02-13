07ad9595f3d31269ee870587737bec27d0d201ac
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-02-13 11:27:43 -0800

    can you make sure my syntax is right :o

--- frontend/src/about/About.js
+++ frontend/src/about/About.js
@@ -9,7 +9,24 @@ import './About.scss';
 const About = () => {
   return (
     <Container fluid className="about-page">
-      <h1>About</h1>
+      <Row>
+        <Col> <h1 className='company-name'>CastAway</h1> </Col>
+      </Row>
+      <Row>
+        <Col> <h2> About CastAway </h2> </Col>
+        <Col> <h2> The Team </h2> </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p className="lit">
+            its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit its lit
+          </p>
+        </Col>
+        <Col>
+          <Row> </Row>
+          <Row> </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
