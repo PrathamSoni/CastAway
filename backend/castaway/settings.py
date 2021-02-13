84ee45cb07817014ca542914f4ec6bfd306ecb3c
PrathamSoni <psoni2002@gmail.com>
2021-02-13 12:49:51 -0500

    ok

--- backend/castaway/settings.py
+++ backend/castaway/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -187,3 +187,4 @@ USE_TZ = True
 
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
