e4b0b245e3b379760bb8a0e79a90fcfd8c387c21
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-02-13 15:27:14 -0800

    can you see if i added the images correctly lol

---
+++ frontend/src/about/About.img/index.js
@@ -0,0 +1,12 @@
+import d from "./dev.png";
+import s from "./samarth.png";
+import n from "./naomi.png";
+import p from "./prath.png";
+
+const images = {
+    d,
+    s,
+    p,
+    n
+}
+export default images;
--- frontend/src/about/About.js
+++ frontend/src/about/About.js
@@ -3,17 +3,14 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import images from './About.img/index.js';
 import './About.scss';
 
 const About = () => {
   return (
     <Container fluid className="about-page">
       <Row>
-        <Col> <h1 className='company-name'>CastAway</h1> </Col>
-      </Row>
-      <Row>
-        <Col> <h2 className='sub-heading'> About CastAway </h2> </Col>
+        <Col> <h1 className='heading'> About CastAway </h1> </Col>
         <Col> <h2 className='sub-heading'> The Team </h2> </Col>
       </Row>
       <Row>
@@ -24,12 +21,16 @@ const About = () => {
         </Col>
         <Col>
           <Row>
-            <h3 className="names"> Devarth Iyer </h3>
-            <h3 className="names"> Samarth Goel </h3>
+            <img src={images.d} alt='Devrath Iyer'/>
+              <h3 className="names"> Devrath Iyer </h3>
+            <img src={images.s} alt='Samarth Goel'/>
+              <h3 className="names"> Samarth Goel </h3>
           </Row>
           <Row>
-            <h3 className="names"> Prathman Soni </h3>
-            <h3 className="names"> Naomi Liu </h3>
+            <img src={images.p} alt='Prathman Soni'/>
+              <h3 className="names"> Prathman Soni </h3>
+            <img src={images.n} alt='Naomi Liu'/>
+              <h3 className="names"> Naomi Liu </h3>
           </Row>
         </Col>
       </Row>
