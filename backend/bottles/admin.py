1f4cc027de3d935a2fc0be71944c82a5ee679a47
Devrath Iyer <deviyer@stanford.edu>
2021-02-13 11:36:06 -0700

    added sent and recieved routes

--- backend/bottles/admin.py
+++ backend/bottles/admin.py
@@ -4,5 +4,6 @@ from .models import Message
 
 class BottleAdmin(admin.ModelAdmin):
     """ The Topic model admin. """
+    list_display = ("sender", "recipient")
 
 admin.site.register(Message, BottleAdmin)
