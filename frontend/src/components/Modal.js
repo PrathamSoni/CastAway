d79fbda37f945a47ca88b266d9cc68fac30fcc11
Devrath Iyer <deviyer@stanford.edu>
2021-02-13 23:00:30 -0700

    added replying

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
   Modal,
@@ -10,22 +10,46 @@ import {
   ModalCloseButton,
   Button,
 } from '@chakra-ui/react';
+import { Row } from 'react-bootstrap';
 
-const SiteModal = ({ isOpen, onClose, title, body, reply }) => {
+const SiteModal = ({ isOpen, onClose, title, body, reply, submitReply }) => {
+
+  const [replying, setReplying] = useState(false);
+  const [replyText, setReplyText] = useState("");
+
+  const closeHandler = () => {
+    setReplying(false);
+    setReplyText("");
+    onClose();
+  };
+
+  const replyHandler = (replyText) => {
+    setReplying(false);
+    submitReply(replyText);
+    setReplyText("");
+    onClose();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={closeHandler}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>{title}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>{body}</ModalBody>
+        <ModalBody>{body}
+        {replying && 
+            <Row>
+              <textarea value={replyText} className="w-100 m-2 p-1" onChange={(e) => setReplyText(e.target.value)}/>
+            </Row>
+           }</ModalBody>
 
         <ModalFooter px={4} py={2}>
-          {reply && <Button variant="ghost">Reply</Button>}
-          <Button colorScheme="blue" ml={3} onClick={onClose}>
+          {reply && <Button onClick={!replying ? (() => setReplying(true)):(() => replyHandler(replyText))} setvariant="ghost">{replying ? "Send":"Reply"}</Button>}
+          <Button colorScheme="blue" ml={3} onClick={closeHandler}>
             Close
           </Button>
         </ModalFooter>
+        
       </ModalContent>
     </Modal>
   );
