307a7f233a3600306bede08c9005d420162bd4ae
Samarth Goel <samarthgoel@Samarths-Air.hsd1.il.comcast.net>
2021-02-13 18:20:38 -0600

    updated styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,17 +3,26 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
+import Logo from '../logo.png';
+
 import './Navbar.scss';
 
 const SiteNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg" className="navbar">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="lg" className="navbar">
+      <Navbar.Brand href="/" className="brand-container">
+        <img src={Logo} alt="" />
+        <p className="logo">Castaway</p>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto nav">
-          <Nav.Link href="#home">Sign Up</Nav.Link>
-          <Nav.Link href="#link">Login</Nav.Link>
+          <Nav.Link href="#home">
+            <p>Sign Up</p>
+          </Nav.Link>
+          <Nav.Link href="#link">
+            <p>Login</p>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
