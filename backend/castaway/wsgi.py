14d214633e31db3450b72897a34b3ee9f910afce
PrathamSoni <psoni2002@gmail.com>
2021-02-14 12:14:08 -0500

    git ignore master

--- backend/castaway/wsgi.py
+++ backend/castaway/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from dotenv import load_dotenv
+project_folder = os.getcwd()
+load_dotenv(os.path.join(project_folder,'.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'castaway.settings')
 
 application = get_wsgi_application()
